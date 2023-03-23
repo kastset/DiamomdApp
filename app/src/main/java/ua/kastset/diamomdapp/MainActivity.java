7ddcb1c7f813406e0e6d7511c22e66f6cb065744
KastSet <stupakkostya23@hmail.com>
2023-03-23 18:40:29 +0200

    try created normal name

--- app/src/main/java/ua/kastset/diamomdapp/MainActivity.java
+++ app/src/main/java/ua/kastset/diamomdapp/MainActivity.java
@@ -17,13 +17,12 @@ import ua.kastset.diamomdapp.design.GridViewAdapter;
 import ua.kastset.diamomdapp.model.ArrayListCardType;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
-    private static int allCard = 35;
-    private static int allUseCard = allCard;
+    private static int entireDeckOfCards = 35;
+    private static int allCardsPlayed = entireDeckOfCards;
     private static int badNumber = 2;
     private static int amountRelic = 0;
-    public static double probability;
-    List<Card> board = new ArrayList<>();
-    boolean isEnd;
+    public static double losingChance;
+    List<Card> gameBoard = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,33 +42,32 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Card type = (Card) parent.getItemAtPosition(position);
-        if (!type.isDead) {
-            --allUseCard;
-            if (type.getCards().equals("Treasure")) {
-                probability = ((double) (badNumber) / allUseCard);
-                String mistakeToLose = String.valueOf(probability);
-                Toast.makeText(MainActivity.this, type.playCard() + mistakeToLose, Toast.LENGTH_SHORT).show();
+        Card playerAction = (Card) parent.getItemAtPosition(position);
+        if (!playerAction.isDead) {
+            --allCardsPlayed;
+            if (playerAction.getCards().equals("Treasure")) {
+                Toast.makeText(MainActivity.this, playerAction.playCard(), Toast.LENGTH_SHORT).show();
+                losingChance = ((double) (badNumber) / allCardsPlayed) * 100;
+                String mistakeToLose = String.format("%.1f", losingChance) ;
+                Toast.makeText(MainActivity.this, mistakeToLose + "%", Toast.LENGTH_SHORT).show();
             } else {
-                Toast.makeText(MainActivity.this, type.playCard(), Toast.LENGTH_SHORT).show();
-                for (Card card : board) {
-                    if(card.getCards().equals(type.getCards())) {
+                Toast.makeText(MainActivity.this, playerAction.playCard(), Toast.LENGTH_SHORT).show();
+                for (Card card : gameBoard) {
+                    if(card.getCards().equals(playerAction.getCards())) {
                         Toast.makeText(MainActivity.this, "you dead", Toast.LENGTH_SHORT).show();
-                        isEnd = true;
                     }
                 }
-                probability = ((double) (badNumber) / allUseCard);
-                String mistakeToLose = String.valueOf(probability);
-                Toast.makeText(MainActivity.this, mistakeToLose, Toast.LENGTH_SHORT).show();
+                losingChance = ((double) (badNumber) / allCardsPlayed) * 100;
+                String mistakeToLose = String.format("%.1f", losingChance);
+                Toast.makeText(MainActivity.this, mistakeToLose + "%", Toast.LENGTH_SHORT).show();
                 badNumber +=2 ;
-                board.add(type);
+                gameBoard.add(playerAction);
             }
         }
     }
 
     public void createNewRound () {
-        allUseCard = allCard - amountRelic;
-        board.clear();
-        isEnd = false;
+        allCardsPlayed = entireDeckOfCards - amountRelic;
+        gameBoard.clear();
     }
 }
